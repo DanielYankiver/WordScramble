89071a926abaa2ea15c18b4ea97624a4a159e773
Daniel Yankiver <dyankiver@gmail.com>
2023-12-19 10:48:10 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Daniel Yankiver on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
